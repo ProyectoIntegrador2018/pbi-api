31a09dbc6c197d6022b492be5f9e609215bceac7
davreds <dav_rojas@hotmail.com>
2020-05-25 01:14:00 -0500

    Created endpoint to get all appointments during a certain timespan. Modified Appointment model to track the parent Record's id

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -3,9 +3,13 @@ const Record = require('../models/record')
 const User = require('../models/record')
 
 const createAppointment = function(req,res){
-    const appointment = new Appointment(req.body)
+    var appointment = new Appointment(req.body)
+    appointment.record = req.params.id
     appointment.save().then(()=>{
         Record.findById(req.params.id).then((record)=>{
+            if(!record){
+                return res.status(404).send({error:"Expediente no encontrado"})
+            }
             record.appointments.push(appointment._id)
             record.save().then(()=>{
                 return res.send({record:record,appointment:appointment})
@@ -14,7 +18,6 @@ const createAppointment = function(req,res){
                 return res.status(400).send({error:"Hubo un error, intentalo de nuevo"})
             })
         }).catch(error=>{
-            
             return res.status(500).send({error:"No se pudo guardar la cita"})
         })
     })
@@ -25,7 +28,7 @@ const getAppointment = function(req,res){
     const id = req.params.id
     Appointment.findById(id).then((appoint)=>{
         if(!appoint){
-            return res.status(400).send({error:"Cita no encontrada"})
+            return res.status(404).send({error:"Cita no encontrada"})
         }
         return res.send(appoint)
     }).catch(()=>{
@@ -79,10 +82,21 @@ const updateAppointment = function(req,res){
     })
 }
 
+const getAppointmentsSpan = function(req, res){
+    const _start = new Date(req.body.startDate)
+    const _end = new Date(req.body.endDate+'T23:59:59.000Z')
+    Appointment.find({'date': {$gte: _start, $lte: _end}}).populate('record').then(function(appointments){
+        res.send(appointments)
+    }).catch(function(error){
+        return res.status(500).send({error: error})
+    })
+}
+
 module.exports = {
     createAppointment:createAppointment,
     deleteAppointment:deleteAppointment,
     updateAppointment:updateAppointment,
     getAppointment:getAppointment,
-    getAppointments:getAppointments
+    getAppointments:getAppointments,
+    getAppointmentsSpan: getAppointmentsSpan
 }
