fb90b34f77a2ef8bc7ee55ecd81cc93d86815a2c
Rick <garzag.rick@gmail.com>
2020-09-30 20:24:12 -0500

    Agrega activación de cuentas como nutricionistas

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.CONNECTION_URL = process.env.CONNECTION_URL || "mongodb://localhost/pbidb";
+exports.CONNECTION_URL = process.env.CONNECTION_URL || "mongodb://localhost:27017/pbidb";
 
 exports.FRONT_URL = process.env.FRONT_URL || "localhost:8080";
 
@@ -15,3 +15,5 @@ exports.HOST = process.env.HOST || "smtp.mail.com";
 exports.PORT = process.env.PORT || 3000;
 
 exports.SECURE = process.env.SECURE || false;
+
+exports.NEW_NUTRI_CODE = process.env.NUTRI_CODE || "secretToken";
